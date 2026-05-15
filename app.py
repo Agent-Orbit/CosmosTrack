d0b2d03cdb8cff6a44a84e357abe2d3147d8abe3
AliAkbar4025 <syedaliakbar40.25@gmail.com>
2026-05-15 16:26:00 +0500

    .

---
+++ app.py
@@ -0,0 +1,19 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="CosmosTrack",
+    page_icon="🚀",
+    layout="wide"
+)
+
+# Navigation
+pages = {
+    "🌌 CosmosTrack": [
+        st.Page("pages/1_APOD.py", title="Picture of the Day", icon="📸"),
+        st.Page("pages/2_Asteroids.py", title="Asteroid Tracker", icon="☄️"),
+        st.Page("pages/3_ISS_Tracker.py", title="ISS Live Tracker", icon="🛸"),
+    ]
+}
+
+pg = st.navigation(pages)
+pg.run()
